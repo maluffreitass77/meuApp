c04a297cdae298495992e54b2614ca79f67ae17b
Maria Luiza de Freitas <maria.freitas4@alunos.sc.senac.br>
2026-03-11 14:00:51 -0300

    olá

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,33 +1,29 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+<ion-page>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+<ion-header>
+  <ion-toolbar>
+    <ion-title>Gerenciador de Tarefas</ion-title>
+  </ion-toolbar>
+</ion-header>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-       <ion-button @click="irParaTarefas">
+<ion-content class="ion-padding">
+
+  <h2>Bem-vindo!</h2>
+  <p>Este aplicativo permite adicionar e organizar suas tarefas.</p>
+
+  <ion-button expand="block" @click="irParaTarefas">
     Ir para Tarefas
   </ion-button>
-    </ion-content>
-   </ion-page>
+
+</ion-content>
+
+</ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue'
 import { useIonRouter } from '@ionic/vue'
-import { IonButton } from '@ionic/vue'
 
 const router = useIonRouter()
 
@@ -35,33 +31,3 @@ const irParaTarefas = () => {
   router.push('/tarefas')
 }
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
--- src/views/TarefasPage.vue
+++ src/views/TarefasPage.vue
@@ -1,20 +1,96 @@
 <template>
-
 <ion-page>
 
 <ion-header>
   <ion-toolbar>
+
+    <ion-buttons slot="start">
+      <ion-back-button default-href="/" />
+    </ion-buttons>
+
     <ion-title>Tarefas</ion-title>
+
   </ion-toolbar>
 </ion-header>
 
-<ion-content>
-  Página de Tarefas
+<ion-content class="ion-padding">
+
+  <!-- Campo para nova tarefa -->
+
+  <ion-item>
+    <ion-input
+      v-model="novaTarefa"
+      placeholder="Digite uma tarefa">
+    </ion-input>
+  </ion-item>
+
+  <ion-button expand="block" @click="adicionarTarefa">
+    Adicionar
+  </ion-button>
+
+  <!-- Estado vazio -->
+
+  <p v-if="tarefas.length === 0">
+    Nenhuma tarefa cadastrada. Adicione a primeira!
+  </p>
+
+  <!-- Lista de tarefas -->
+
+  <ion-list v-else>
+
+    <ion-item v-for="(tarefa, index) in tarefas" :key="index">
+
+      <ion-label>
+        {{ tarefa }}
+      </ion-label>
+
+      <ion-button
+        color="danger"
+        fill="clear"
+        @click="removerTarefa(index)">
+        Remover
+      </ion-button>
+
+    </ion-item>
+
+  </ion-list>
+
 </ion-content>
 
 </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'
+import {
+IonPage,
+IonHeader,
+IonToolbar,
+IonTitle,
+IonContent,
+IonItem,
+IonInput,
+IonButton,
+IonList,
+IonLabel,
+IonButtons,
+IonBackButton
+} from '@ionic/vue'
+
+import { ref } from 'vue'
+
+const novaTarefa = ref('')
+const tarefas = ref<string[]>([])
+
+const adicionarTarefa = () => {
+
+  if (novaTarefa.value.trim() === '') return
+
+  tarefas.value.push(novaTarefa.value)
+
+  novaTarefa.value = ''
+}
+
+const removerTarefa = (index:number) => {
+  tarefas.value.splice(index, 1)
+}
 </script>
